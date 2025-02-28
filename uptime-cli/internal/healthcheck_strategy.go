86f1757ce151a2c9d5170cfb5a5e911486ee65be
mskreczko <mic.skreczko@gmail.com>
2025-02-28 20:01:31 +0100

    add healthcheck strategy for status code

--- uptime-cli/internal/healthcheck_strategy.go
+++ uptime-cli/internal/healthcheck_strategy.go
@@ -41,12 +41,25 @@ func (s *RawStringHealthCheckStrategy) healthcheck(url url.URL) bool {
 	return strings.Contains(string(body), "UP")
 }
 
+type StatusHealthCheckStrategy struct{}
+
+func (s *StatusHealthCheckStrategy) healthcheck(url url.URL) bool {
+	res, err := http.Get(url.String())
+	if err != nil {
+		fmt.Printf("Error making request to %s: %s\n", url.String(), err)
+		return false
+	}
+	return res.StatusCode == 200
+}
+
 func ResolveHealthCheckStrategy(strategy string) HealthCheckStrategy {
 	switch strategy {
 	case "json":
 		return &JSONHealthCheckStrategy{}
 	case "raw":
 		return &RawStringHealthCheckStrategy{}
+	case "status_code":
+		return &StatusHealthCheckStrategy{}
 	}
 	return nil
 }
